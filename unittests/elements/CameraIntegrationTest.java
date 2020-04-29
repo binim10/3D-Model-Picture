bd5e17d36fc109a1f48e29fbbd33cfef8c9459e0
zeev abramovich <57068932+zeviAbramovich@users.noreply.github.com>
2020-04-29 15:04:47 +0300

    6.9

--- unittests/elements/CameraIntegrationTest.java
+++ unittests/elements/CameraIntegrationTest.java
@@ -25,6 +25,7 @@ public class CameraIntegrationTest {
     int count = 0;
     int Nx = 3;
     int Ny = 3;
+
     /**
      * Tests for sphere integration with find intersections method
      */
@@ -39,13 +40,13 @@ public class CameraIntegrationTest {
                     count += results.size();
             }
         }
-        assertEquals("SphTC01:bad ray",2, count);
+        assertEquals("SphTC01:bad ray", 2, count);
         System.out.println("count: " + count);
 
         //TC02: 18 intersections.
         Sphere sph2 = new Sphere(2.5, new Point3D(0, 0, 2.5));
 
-        results=null;
+        results = null;
         count = 0;
         for (int i = 0; i < Ny; ++i) {
             for (int j = 0; j < Nx; ++j) {
@@ -54,14 +55,14 @@ public class CameraIntegrationTest {
                     count += results.size();
             }
         }
-        assertEquals("SphTC02:bad Ray",18,count);
+        assertEquals("SphTC02:bad Ray", 18, count);
         System.out.println("count: " + count);
 
         //TC03:10 intersection points
-        Sphere sph3=new Sphere(2, new Point3D(0, 0, 2));
+        Sphere sph3 = new Sphere(2, new Point3D(0, 0, 2));
 
-        results=null;
-        count=0;
+        results = null;
+        count = 0;
         for (int i = 0; i < Ny; ++i) {
             for (int j = 0; j < Nx; ++j) {
                 results = sph3.findIntersections(new Ray(cam2.constructRayThroughPixel(Nx, Ny, j, i, 1, 3, 3)));
@@ -69,14 +70,14 @@ public class CameraIntegrationTest {
                     count += results.size();
             }
         }
-        assertEquals("SphTC03:bad Ray",10,count);
+        assertEquals("SphTC03:bad Ray", 10, count);
         System.out.println("count: " + count);
 
         //TC04:9 intersection points
-        Sphere sph4=new Sphere(4, new Point3D(0, 0, 2));
+        Sphere sph4 = new Sphere(4, new Point3D(0, 0, 2));
 
-        results=null;
-        count=0;
+        results = null;
+        count = 0;
         for (int i = 0; i < Ny; ++i) {
             for (int j = 0; j < Nx; ++j) {
                 results = sph4.findIntersections(new Ray(cam1.constructRayThroughPixel(Nx, Ny, j, i, 1, 3, 3)));
@@ -84,14 +85,14 @@ public class CameraIntegrationTest {
                     count += results.size();
             }
         }
-        assertEquals("SphTC03:bad Ray",9,count);
+        assertEquals("SphTC03:bad Ray", 9, count);
         System.out.println("count: " + count);
 
         //TC05:Zero intersection points
-        Sphere sph5=new Sphere(0.5, new Point3D(0, 0, -1));
+        Sphere sph5 = new Sphere(0.5, new Point3D(0, 0, -1));
 
-        results=null;
-        count=0;
+        results = null;
+        count = 0;
         for (int i = 0; i < Ny; ++i) {
             for (int j = 0; j < Nx; ++j) {
                 results = sph5.findIntersections(new Ray(cam1.constructRayThroughPixel(Nx, Ny, j, i, 1, 3, 3)));
@@ -99,7 +100,7 @@ public class CameraIntegrationTest {
                     count += results.size();
             }
         }
-        assertEquals("SphTC05:bad Ray",0,count);
+        assertEquals("SphTC05:bad Ray", 0, count);
         System.out.println("count: " + count);
         //END
     }
@@ -110,9 +111,9 @@ public class CameraIntegrationTest {
     @Test
     public void testConstructRayThroughPixelWithPlane() {
         //TC01:9 intersection points
-        Plane pla1=new Plane(new Point3D(0,0,3),new Point3D(0,1,3),new Point3D(1,0,3));
-        results=null;
-        count=0;
+        Plane pla1 = new Plane(new Point3D(0, 0, 3), new Point3D(0, 1, 3), new Point3D(1, 0, 3));
+        results = null;
+        count = 0;
         for (int i = 0; i < Ny; ++i) {
             for (int j = 0; j < Nx; ++j) {
                 results = pla1.findIntersections(new Ray(cam1.constructRayThroughPixel(Nx, Ny, j, i, 1, 3, 3)));
@@ -120,13 +121,13 @@ public class CameraIntegrationTest {
                     count += results.size();
             }
         }
-        assertEquals("PlaTC01:bad Ray",9,count);
+        assertEquals("PlaTC01:bad Ray", 9, count);
         System.out.println("count: " + count);
 
         //TC02: 9 intersection points
-        Plane pla2=new Plane(new Point3D(0,0,2),new Point3D(0,-4,1),new Point3D(1,4,3));
-        results=null;
-        count=0;
+        Plane pla2 = new Plane(new Point3D(0, 0, 2), new Point3D(0, -4, 1), new Point3D(1, 4, 3));
+        results = null;
+        count = 0;
         for (int i = 0; i < Ny; ++i) {
             for (int j = 0; j < Nx; ++j) {
                 results = pla2.findIntersections(new Ray(cam1.constructRayThroughPixel(Nx, Ny, j, i, 1, 3, 3)));
@@ -134,13 +135,13 @@ public class CameraIntegrationTest {
                     count += results.size();
             }
         }
-        assertEquals("PlaTC02:bad Ray",9,count);
+        assertEquals("PlaTC02:bad Ray", 9, count);
         System.out.println("count: " + count);
 
         //TC03:6 intersection points
-        Plane pla3=new Plane(new Point3D(0,0,2),new Point3D(0,-1,1),new Point3D(1,1,3));
-        results=null;
-        count=0;
+        Plane pla3 = new Plane(new Point3D(0, 0, 2), new Point3D(0, -1, 1), new Point3D(1, 1, 3));
+        results = null;
+        count = 0;
         for (int i = 0; i < Ny; ++i) {
             for (int j = 0; j < Nx; ++j) {
                 results = pla3.findIntersections(new Ray(cam1.constructRayThroughPixel(Nx, Ny, j, i, 1, 3, 3)));
@@ -148,7 +149,7 @@ public class CameraIntegrationTest {
                     count += results.size();
             }
         }
-        assertEquals("PlaTC03:bad Ray",6,count);
+        assertEquals("PlaTC03:bad Ray", 6, count);
         System.out.println("count: " + count);
 
         //END
@@ -160,9 +161,9 @@ public class CameraIntegrationTest {
     @Test
     public void testConstructRayThroughPixelWithTriangle() {
         //TC01: One intersection point
-        Triangle triangle = new Triangle(new Point3D(0,-1,2),new Point3D(1, 1, 2),new Point3D(-1,1,2));
-        results=null;
-        count=0;
+        Triangle triangle = new Triangle(new Point3D(0, -1, 2), new Point3D(1, 1, 2), new Point3D(-1, 1, 2));
+        results = null;
+        count = 0;
         for (int i = 0; i < Ny; ++i) {
             for (int j = 0; j < Nx; ++j) {
                 results = triangle.findIntersections(new Ray(cam2.constructRayThroughPixel(Nx, Ny, j, i, 1, 3, 3)));
@@ -170,13 +171,13 @@ public class CameraIntegrationTest {
                     count += results.size();
             }
         }
-        assertEquals("TriTC01:bad Ray",1,count);
+        assertEquals("TriTC01:bad Ray", 1, count);
         System.out.println("count: " + count);
 
         //TC02: Two intersection points
-        Triangle triangle2 = new Triangle(new Point3D(0,-20,2),new Point3D(1, 1, 2),new Point3D(-1,1,2));
-        results=null;
-        count=0;
+        Triangle triangle2 = new Triangle(new Point3D(0, -20, 2), new Point3D(1, 1, 2), new Point3D(-1, 1, 2));
+        results = null;
+        count = 0;
         for (int i = 0; i < Ny; ++i) {
             for (int j = 0; j < Nx; ++j) {
                 results = triangle2.findIntersections(new Ray(cam1.constructRayThroughPixel(Nx, Ny, j, i, 1, 3, 3)));
@@ -184,7 +185,7 @@ public class CameraIntegrationTest {
                     count += results.size();
             }
         }
-        assertEquals("TriTC02:bad Ray",2,count);
+        assertEquals("TriTC02:bad Ray", 2, count);
         System.out.println("count: " + count);
         //END
     }
