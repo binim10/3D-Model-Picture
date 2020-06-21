c2588f61f07c736aeedaa53fd6b0b619b9255564
zeev abramovich <57068932+zeviAbramovich@users.noreply.github.com>
2020-06-21 21:48:18 +0300

    ata totah

--- unittests/renderer/improveSpeed.java
+++ unittests/renderer/improveSpeed.java
@@ -26,7 +26,7 @@ public class improveSpeed {
         Geometries aRight = new Geometries(), bLeft = new Geometries(), c = new Geometries();
         for (int i = 1; i < 300; i++) {
             aRight.add(new Sphere(new Color(100 * 5 / i + 1, 100 * 3 / i + 1.5, 100 * 6 / i * 0.8), new Material(0.2, 0.2, 200, 0, getRandom(0.1, 0.8)), // )
-                    80, new Point3D(100 * i / 5, 2, 10000)));
+                    50 , new Point3D(-600+i*10, -600+i*10, 10000)));
 //                    new Sphere(new Color(800, 0, 0), new Material(0.5, 0.5, 200, 0.5, 0), // )
 //                            140, new Point3D(260, 260, 500)),
 //                    new Sphere(new Color(0, 0, 200), new Material(0.25, 0.25, 20, 0, 0.5), // )
@@ -37,7 +37,7 @@ public class improveSpeed {
         }
         for (int i = 1; i < 300; i++) {
             bLeft.add(new Sphere(new Color(100 * 5 / i + 1, 100 * 3 / i + 1.5, 100 * 6 / i * 0.8), new Material(0.5, 0.5, 200, 0.5, 0), // )
-                    100, new Point3D(-10 * i / 5, 30, 10000)));
+                    50, new Point3D(300+i*5, 400+i*5, 10000)));
         }
         bLeft.add(new Triangle(new Color(getRandom(1, 255), getRandom(1, 250), getRandom(1, 255)), new Material(0.5, 0.5, 100, 0.5, 0.5),
                 new Point3D(-100, 400, 150), new Point3D(100, 400, 350), new Point3D(0, 200, 250)));
@@ -49,7 +49,7 @@ public class improveSpeed {
                         new Point3D(0, 0, -1500), 1, 4E-5, 2E-7, new Vector(0, 0, 1)).setRadius(10),
                 new PointLight(new Color(java.awt.Color.white), new Point3D(0.001, -100, 499), 1, 4E-5, 2E-7).setRadius(9));
 
-        ImageWriter imageWriter = new ImageWriter("The magical room with BVH 601Bodies", 200, 200, 600, 600);
+        ImageWriter imageWriter = new ImageWriter("The magical room with BVH 601Bodies", 200, 200, 1000, 1000);
         Render render = new Render(imageWriter, scene).setSuperSampling(200).setMultithreading(3).setDebugPrint();
 
         render.renderImage();
